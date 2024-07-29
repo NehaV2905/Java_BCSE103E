9cb05ce9ea28e6d48a0c8de1aac78f3d1ace16d7
Neha <nehav3j@gmail.com>
2024-07-29 16:59:53 +0530

    Added codes done on 29.07.24 in lab

---
+++ week3/mon_29.07.24/StringBuilderAppend.java
@@ -0,0 +1,8 @@
+public class StringBuilderAppend{
+	public static void main(String[] args){
+		StringBuilder sb = new StringBuilder("Hello ");
+		sb.append("Java");
+		System.out.println(sb);
+	}
+}
+
